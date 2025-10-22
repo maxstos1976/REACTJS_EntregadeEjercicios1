9f6d5a5e63251a52a60c9dba4762fecccf1a4ac0
Max Coelho dos Santos <maxstos@gmail.com>
2025-10-22 14:41:02 +0200

    descripcionEjercicio2

--- contador-clics/src/App.jsx
+++ contador-clics/src/App.jsx
@@ -9,11 +9,24 @@ function App() {
   };
 
   return (
-    <div>
-      <button onClick={contarClics}>Contar Clics</button>
-      <p>
-        Cuantidad de clics: <span>{clics}</span>
-      </p>
+    <div className="principal">
+      <div className="descripcion">
+        <h1>2. Contador de Clics</h1>
+        <h3>
+          Objetivo del ejercicio: Practicar el manejo de eventos y la
+          actualización del contenido del DOM.
+        </h3>
+        <h3>
+          Ejercicio: Crea una página con un botón que diga "Contar clics" y un
+          texto inicial que muestre "Clics: 0". Cada vez que se haga clic en el
+          botón, el texto debe actualizarse para mostrar el número total de
+          clics realizados.
+        </h3>
+        <button onClick={contarClics}>Contar Clics</button>
+        <p>
+          Clics: <span>{clics}</span>
+        </p>
+      </div>
     </div>
   );
 }
