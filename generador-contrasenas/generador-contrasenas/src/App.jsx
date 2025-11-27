850d404d7a18635f7ad96260d2a16861373e4ce0
Max Coelho dos Santos <maxstos@gmail.com>
2025-11-27 10:24:04 +0100

    generador_contrasena_update

--- generador-contrasenas/generador-contrasenas/src/App.jsx
+++ generador-contrasenas/generador-contrasenas/src/App.jsx
@@ -1,10 +1,49 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [tamanho, setTamanho] = useState(8); // estado para o tamanho da senha
+  const [senha, setSenha] = useState("");
+
+  const caracteresMaiusculasAceitos = ["ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789"];
+  const caracteresEspeciaisAceitos = ["!@#$%^&*()_-+="];
+  const caracteresMinusculasAceitos = caracteresMaiusculasAceitos.map((s) =>
+    s.toLowerCase()
+  );
+
+  const caracteresAceitosTodos = [
+    ...caracteresMaiusculasAceitos[0].split(""),
+    ...caracteresMinusculasAceitos[0].split(""),
+    ...caracteresEspeciaisAceitos[0].split(""),
+  ];
+
+  const gerarContrasena = (tamano) => {
+    let resultado = "";
+    const totalCaracteres = caracteresAceitosTodos.join("").length;
+    console.log(totalCaracteres);
+    for (let i = 0; i < tamano; i++) {
+      const indiceRandom = Math.floor(Math.random() * totalCaracteres);
+      resultado += caracteresAceitosTodos[indiceRandom];
+    }
+    return resultado;
+  };
+
   return (
-    <>
-      <p>Generador Contraseñas</p>
-    </>
+    <div className="principal">
+      <h1>Generador Contraseñas</h1>
+      <label>
+        Número de caracteres:
+        <input
+          type="number"
+          min="8"
+          value={tamanho}
+          onChange={(e) => setTamanho(Number(e.target.value))}
+        />
+      </label>
+      <button onClick={() => setSenha(gerarContrasena(tamanho))}>
+        Generar Contraseña
+      </button>
+      <span>{senha}</span>
+    </div>
   );
 }
 
